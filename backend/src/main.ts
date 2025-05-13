3fdcd9e1a88074d45e179e7a01805a0e90c5c888
willian-mayer <willian.mayer.br@gmail.com>
2025-05-13 15:59:21 -0300

    allowing CORS to frontend.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // o '*', si prefieres permitir todos (no recomendado en prod)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
